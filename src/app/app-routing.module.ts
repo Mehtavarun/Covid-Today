d01b4faafd0544c5775faf6e143b2b545391c324
Varun Mehta <mehtavarun96@rediffmail.com>
2020-04-26 13:12:39 +0530

    web-api-module added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { PrecautionsComponent } from './precautions/precautions.component';
-import { LatestNewsComponent } from './latest-news/latest-news.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LogoutComponent } from './logout/logout.component';
-import { AddNewsComponent } from './add-news/add-news.component';
 import { AdminLoginService } from './services/login/admin/admin-login.service';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { LatestNewsComponent } from './latest-news/latest-news.component';
+import { AddNewsComponent } from './add-news/add-news.component';
+import { AuthGuardService } from './services/auth-guard/auth-guard.service';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'dashboard',
+    redirectTo: '/dashboard',
     pathMatch: 'full'
   },
   {
@@ -31,22 +32,27 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {
-    path: 'news',
-    component: LatestNewsComponent,
-    pathMatch: 'full',
-    canActivateChild: [AdminLoginService],
-    children: [
-      {
-        path: 'add',
-        component: AddNewsComponent,
-        pathMatch: 'full'
-      }
-    ]
-  },
-  {
     path: 'precautions',
     component: PrecautionsComponent,
     pathMatch: 'full'
+  },
+  {
+    path: 'add',
+    component: AddNewsComponent,
+    pathMatch: 'full',
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'news',
+    component: LatestNewsComponent,
+    pathMatch: 'full',
+    canActivateChild: [AuthGuardService],
+    children: [
+      {
+        path: 'add',
+        component: AddNewsComponent
+      }
+    ]
   },
   {
     path: '**',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,10 +15,17 @@ import { TitleBarComponent } from './title-bar/title-bar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LatestNewsComponent } from './latest-news/latest-news.component';
 import { PrecautionsComponent } from './precautions/precautions.component';
-import { ToastrModule } from 'ngx-toastr';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AddNewsComponent } from './add-news/add-news.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { DataService } from './services/data.service';
+import { LoaderComponent } from './shared/loader/loader.component';
+import { LoaderService } from './services/loader/loader.service';
+import { LoaderInterceptor } from './shared/loader/loader.intercepter';
+
+const webApiConfig = {
+  delay: 500,
+  passThruUnknownUrl: true
+};
 
 @NgModule({
   declarations: [
@@ -24,15 +37,22 @@ import { NotFoundComponent } from './not-found/not-found.component';
     LatestNewsComponent,
     PrecautionsComponent,
     AddNewsComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ToastrModule.forRoot(),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    InMemoryWebApiModule.forRoot(DataService, webApiConfig)
   ],
-  providers: [],
+  providers: [
+    LoaderService,
+    { provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
